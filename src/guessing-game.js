ff41b75d15c6eb71a5d591d2668682cbd09bd6f9
Anna Khamieva <annakhmv@gmail.com>
2022-08-16 22:55:51 +0500

    Feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        return this.max = Math.round((this.max + this.min) / 2) ;
     }
 
     greater() {
-
+        return this.min = Math.round((this.max + this.min) / 2);
     }
+    
 }
 
 module.exports = GuessingGame;
